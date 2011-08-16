46fced534b040196e2f43641e6fbe4f4b4899e5f
yoshi@practice <yoshi.forgit@gmail.com>
2011-08-17 02:13:35 +0900

    commit first

---
+++ src/com/sson/TrainingActivity.java
@@ -0,0 +1,13 @@
+package com.sson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrainingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
